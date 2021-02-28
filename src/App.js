e090ae0f431a56ec76d42e50dc5106c5d4e4a9d3
Kai Dong <dongkai981@gmail.com>
2021-02-28 15:33:54 -0600

    Removed id from employee.json and separated header and table in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import EmployeeTable from "./components/EmployeeTable";
+import TableHeader from "./components/TableHeader";
 import employee from "./employee.json";
 
 class App extends Component {
@@ -8,22 +9,18 @@ class App extends Component {
     employee
   };
 
-  // removeFriend = id => {
-  //   // Filter this.state.employee for employee with an id not equal to the id being removed
-  //   const employee = this.state.employee.filter(friend => friend.id !== id);
-  //   // Set this.state.employee equal to the new employee array
-  //   this.setState({ employee });
-  // };
-
   // Map over this.state.employee and render a FriendCard component for each friend object
   render() {
     return (
       <div>
+      <TableHeader employee = {employee[0]} />
+      <tbody>
         {this.state.employee.map(employee => (
           <EmployeeTable
           employee = {employee}
           />
           ))}
+          </tbody>
       </div>
     );
   }
--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -2,18 +2,11 @@ import React from "react";
 
 
 function EmployeeTable (props) {
+    let employee = props.employee;
+    
     return (
         <table class="table table-striped .thead-light">
             <thread>
-                <tr>
-                    <th scope="col">{employee[key]}</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Occupation</th>
-                    <th scope="col">Location</th>
-                </tr>
-            </thread>
-            <tbody>
                 <tr>
                     <th scope="row">1</th>
                     {Object.keys(employee).map(key => (
@@ -21,7 +14,7 @@ function EmployeeTable (props) {
                     )
                     )}
                 </tr>
-            </tbody>
+            </thread>
         </table>
     )
 }
