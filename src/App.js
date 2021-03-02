e802cb132340780ee9262f82fe4e3fe0d1271ab0
Kai Dong <dongkai981@gmail.com>
2021-03-01 18:53:53 -0600

    Decided to put ID back in the employee.json

--- src/App.js
+++ src/App.js
@@ -6,21 +6,19 @@ import employee from "./employee.json";
 class App extends Component {
   // Setting this.state.employee to the employee json array
   state = {
-    employee
+    employee,
   };
 
   // Map over this.state.employee and render a FriendCard component for each friend object
   render() {
     return (
-      <table className="table table-striped table-dark" >
-      <TableHeader employee = {employee[0]} />
-      <tbody>
-        {this.state.employee.map(employee => (
-          <EmployeeTable
-          employee = {employee}
-          />
+      <table className="table table-striped table-dark">
+        <TableHeader employee={employee[0]} />
+        <tbody>
+          {this.state.employee.map((employee) => (
+            <EmployeeTable employee={employee} />
           ))}
-          </tbody>
+        </tbody>
       </table>
     );
   }
