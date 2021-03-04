5e13f884d9c8fb6ee556770092d2e7567ee89db4
Kai Dong <dongkai981@gmail.com>
2021-03-03 18:13:41 -0600

    Added a search bar above the table

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React, { Component } from "react";
 import EmployeeTable from "./components/EmployeeTable";
 import TableHeader from "./components/TableHeader";
 import employee from "./employee.json";
+import Search from "./components/SearchForm"
 
 class App extends Component {
   // Setting this.state.employee to the employee json array
   state = {
     employee,
+    search: ""
   };
+
+  sortByName = () => {
+    let sortedEmployeeName = this.state.employee.sort((a, b) => (a.name > b.name) ? 1: -1);
+    this.setState({sortedEmployeeName})
+  }
 
   // Map over this.state.employee and render a FriendCard component for each friend object
   render() {
     return (
+      <div>
+      <h2 className="display-3">Employee Directory</h2>
+      <Search />
       <table className="table table-striped table-bordered">
         <TableHeader employee={employee[0]} />
         <tbody>
@@ -20,6 +30,7 @@ class App extends Component {
           ))}
         </tbody>
       </table>
+      </div>
     );
   }
 }
