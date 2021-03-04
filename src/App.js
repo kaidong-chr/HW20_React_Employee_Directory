08cc80759a5137e5ef65e421d422b4e97afdf475
Kai Dong <dongkai981@gmail.com>
2021-03-03 19:35:21 -0600

    Added sorted by location to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TableHeader from "./components/TableHeader";
 import employee from "./employee.json";
 import Search from "./components/SearchForm"
 import SortByNameBtn from "./components/SortByNameBtn"
+import SortByLocationBtn from "./components/SortByLocationBtn"
 
 class App extends Component {
   // Setting this.state.employee to the employee json array
@@ -17,6 +18,11 @@ class App extends Component {
     this.setState({sortedEmployeeName})
   }
 
+  sortByLocation = () => {
+    let sortedEmployeeLocation = this.state.employee.sort((a, b) => (a.location > b.location) ? 1: -1);
+    this.setState({sortedEmployeeLocation})
+  }
+
   // Map over this.state.employee and render a FriendCard component for each friend object
   render() {
     return (
@@ -24,6 +30,7 @@ class App extends Component {
       <h2 className="display-3">Employee Directory</h2>
       <Search />
       <SortByNameBtn sortByName={this.sortByName}/>
+      <SortByLocationBtn sortByLocation={this.sortByLocation}/>
       <table className="table table-striped table-bordered">
         <TableHeader employee={employee[0]} />
         <tbody>
